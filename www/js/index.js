0e0ad24b0647011b21e63b5ab24478afba939535
dbrown1997 <brod2_15@uni.worc.ac.uk>
2016-11-10 10:31:08 +0000

    Revert "WEEK 6"
    This reverts commit 90908d5246a832db3a5c679bb702d3096a7873d4.

--- www/js/index.js
+++ www/js/index.js
@@ -20,24 +20,6 @@ var launched_count = 0;
 
 	var key = "phone";
 	var value = "on";
-	
-//WEEK 6	
-	$(document).on("pagecreate","#pageone",function(){
-  $('#submitButton').on("click", function(){
-    submitText();
-  });            
-});            
-
-
-function submitText() {
-	var text = $('#textinput').val();
-	alert(text);
-}
-
-function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
-}
-//WEEK 6
 
 		window.localStorage.setItem( key, value );
 	
